80a65059631c1b9d6370ed63c57dc25bccbd99c7
ekuzovlev <bio19@yandex.ru>
2023-03-28 23:09:36 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Perfomance.destroy_all
+
+["Ревизор", "Вишневый сад", "День радио"].each do |perfomance|
+  Perfomance.create(title: perfomance)
+end
+
+Calendar.destroy_all
+
+Calendar.create(perfomance: Perfomance.take, duration: Date.new(2019, 2, 20)..Date.new(2019, 3, 1))
